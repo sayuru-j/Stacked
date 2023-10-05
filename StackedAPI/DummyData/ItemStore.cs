ee044f7e6ba6a3daff5f742bc01a2242c39ad66e
sayuru-j <zayuru.ai@gmail.com>
2023-10-05 16:40:31 +0530

    new file:   .gitignore 	new file:   .idea/.idea.Stacked/.idea/.gitignore 	new file:   .idea/.idea.Stacked/.idea/encodings.xml 	new file:   .idea/.idea.Stacked/.idea/indexLayout.xml 	new file:   .idea/.idea.Stacked/.idea/vcs.xml 	new file:   README.md 	new file:   Stacked.sln 	new file:   StackedAPI/Controllers/ItemController.cs 	new file:   StackedAPI/DummyData/ItemStore.cs 	new file:   StackedAPI/Models/Dto/ItemDto.cs 	new file:   StackedAPI/Models/Item.cs 	new file:   StackedAPI/Program.cs 	new file:   StackedAPI/Properties/launchSettings.json 	new file:   StackedAPI/StackedAPI.csproj 	new file:   StackedAPI/appsettings.Development.json 	new file:   StackedAPI/appsettings.json

---
+++ StackedAPI/DummyData/ItemStore.cs
@@ -0,0 +1,12 @@
+﻿using StackedAPI.Models.Dto;
+
+namespace StackedAPI.DummyData;
+
+public class ItemStore
+{
+    public static List<ItemDto> itemList = new List<ItemDto>
+    {
+        new ItemDto { Id = 1, Name = "Headphones" },
+        new ItemDto { Id = 2, Name = "Monitors" }
+    };
+}
